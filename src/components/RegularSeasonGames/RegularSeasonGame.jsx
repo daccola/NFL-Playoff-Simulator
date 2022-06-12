eab61818f922d21f833539bee0bc000592ac45df
daccola <78117344+daccola@users.noreply.github.com>
2022-06-12 15:23:38 -0500

    cleaned up game boxes

--- src/components/RegularSeasonGames/RegularSeasonGame.jsx
+++ src/components/RegularSeasonGames/RegularSeasonGame.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
-import { Adb } from "@mui/icons-material";
-
+import { AlternateEmail} from "@mui/icons-material";
 import { Paper, Grid, Container } from '@mui/material'
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -12,42 +10,33 @@ import Typography from '@mui/material/Typography'
 export default function RegularSeasonGame (props) {
   return (
   	<Card variant="outlined" >
-      <CardContent sx={{p:0,'&:last-child': { pb: 0 }}}>  
-        <Grid container>
-          <Grid item xs={5}>
-            <Card sx={{px:1.5, borderBottom: 4, borderColor: 'red' }}>
-      	      <img src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/det.png" alt="TODO" className = 'col-12'></img>
-            </Card>
-          </ Grid>
-          <Grid item xs={2} >
-            <Card sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              height: '100%'
-            }}>
-            <Adb />
-
-              {/* TODO @ */}
-            </Card>
-          </ Grid>
-          <Grid item xs={5}>
-            <Card sx={{px:1.5, borderBottom: 2, borderColor: 'purple' }}>
-              <img src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/gb.png" alt="TODO" className = 'col-12'></img>
-            </Card>
-          </ Grid> 
-		    </ Grid>
-        <Grid container>
-          <Grid item >
-            <Typography variant="body2" >
-              FINAL
-            </Typography>
-            {/* <AlternateEmailIcon /> */}
-            {/* <svg data-testid="AlternateEmailIcon"></svg> */}
-{/* <AlternateEmailIcon color="primary" /> */}
-
-          </Grid>
+      <Grid container>
+        <Grid item xs={12}>
+          <Card sx={{px:0}}>
+            <Grid container alignItems="center">
+              <Grid item xs={5} sx={{px:1.5, borderBottom: 4, borderColor: 'green'}}>
+                <img src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/min.png" alt="TODO" className = 'col-12'></img>
+              </Grid>
+              <Grid item xs={2} sx={{px:0.25}}>
+                <AlternateEmail sx={{ width: '100%' }} />
+              </Grid>
+              <Grid item xs={5} sx={{px:1.5, borderBottom: 4, borderColor: 'green'}}>
+                <img src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/nfl/500/gb.png" alt="TODO" className = 'col-12'></img>
+              </Grid>
+            </ Grid>
+          </ Card>
+        </ Grid>
+      </ Grid>
+      <Grid container>
+        <Grid item >
+          <Typography variant="body2" >
+            FINAL
+          </Typography>
+          <Typography variant="body2" >
+            December 24, 2022
+          </Typography>
         </Grid>
-      </CardContent>
+      </Grid>
     </Card>
   )  
 }
